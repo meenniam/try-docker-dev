2f0822972e4064c34d064cf45b9fd717dbc5003f
นิอามีน <meen_158@hotmail.com>
2020-03-19 10:21:33 +0700

    update

--- first-api/index.js
+++ first-api/index.js
@@ -8,4 +8,10 @@ app.get('/test',(req, res)=>{
     })
 })
 
+app.get('/test2',(req, res)=>{
+    res.json({
+        data: 'test2'
+    })
+})
+
 app.listen(8080, () => console.log(`Server started on port ${8080}`));
