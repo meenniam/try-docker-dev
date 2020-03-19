81a7ad0cfcfb3c0d368e89467d15a668f2410a9b
นิอามีน <meen_158@hotmail.com>
2020-03-19 10:43:19 +0700

    update

--- second-api/index.js
+++ second-api/index.js
@@ -1,1 +1,17 @@
-console.log('api1')
+const express = require("express");
+
+const app = express();
+
+app.get('/test',(req, res)=>{
+    res.json({
+        data: 'test'
+    })
+})
+
+app.get('/test2',(req, res)=>{
+    res.json({
+        data: 'test2'
+    })
+})
+
+app.listen(8081, () => console.log(`Server started on port ${8081}`));
